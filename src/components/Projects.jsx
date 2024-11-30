97e1edd39ee1b4e61e96226e3bbd2066f7816f18
DivyanshuSonwal <kk5939875@gmail.com>
2024-11-30 20:08:27 +0530

    added-animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,21 @@
 import {PROJECTS} from "../constants/index.js";
+import {motion} from "motion/react";
 
 const Projects = () => {
     return (
         <div className="border-b border-neutral-900 pb-4">
-            <h2 className="my-20 text-center text-4xl">Projects</h2>
+            <motion.h2
+                whileInView={{opacity: 1, y: 0}}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 0.5 }}
+                className="my-20 text-center text-4xl">Projects</motion.h2>
             {PROJECTS.map((project) => (
                 <div key={project?.id} className="mb-8 flex flex-wrap lg:justify-center">
-                    <div className="w-full lg:w-1/4 flex justify-center">
+                    <motion.div
+                        whileInView={{opacity: 1, x: 0}}
+                        initial={{ opacity: 0, x: -100 }}
+                        transition={{duration: 1 }}
+                        className="w-full lg:w-1/4 flex justify-center lg:justify-start">
                         <img
                             src={project?.image}
                             width={150}
@@ -14,8 +23,12 @@ const Projects = () => {
                             alt={project?.title}
                             className="mb-6 rounded"
                         />
-                    </div>
-                    <div className="w-full max-w-xl lg:w-3/4">
+                    </motion.div>
+                    <motion.div
+                        whileInView={{opacity: 1, x: 0}}
+                        initial={{opacity: 0, x: 100}}
+                        transition={{duration: 1 }}
+                        className="w-full max-w-xl lg:w-3/4">
                         <h6 className="mb-2 font-semibold">{project?.title}</h6>
                         <p className="mb-4 text-neutral-400">{project?.description}</p>
                         <div className="flex flex-wrap gap-3">
@@ -26,7 +39,7 @@ const Projects = () => {
                             </span>
                             ))}
                         </div>
-                    </div>
+                    </motion.div>
                 </div>
             ))}
         </div>
